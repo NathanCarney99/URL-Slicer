e062d1a4088cbc969cebec46334ac18f0a6c2481
Nathan <nathan.carney@knights.ucf.edu>
2023-10-23 12:31:11 -0400

    added main.py with firesbase credentials, app routing, and flask setup on main.py

---
+++ server/app/main.py
@@ -0,0 +1,30 @@
+from flask import flask, redirect, render_template
+import firebase_admin 
+from firebase_admin import db
+import os
+
+
+cred_obj = firebase_admin.credentials.Certificate('./ServiceAccountKey.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': 'https://url-shortener-12a39-default-rtdb.firebaseio.com/'
+})
+
+app = flask(__name__, static_folder='./build/static', template__folder='./build')
+
+@app.route("/")
+def hello_world():
+    return redirect("/app")
+
+@app.route("/app")
+def homepage():
+    return render_template("index.html")
+
+@app.route('/path:generatedKey>', methods=['GET'])
+def fetch_from_firebase(generatedKey):
+    ref = db.reference("/"+generatedKey)
+    data = ref.get()
+    if not data:
+        return '404 not found'
+    else: 
+        longURL = data[longURL]
+        return redirect(longURL)
